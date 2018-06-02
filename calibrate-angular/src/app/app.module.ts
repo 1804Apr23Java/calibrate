c25689c320bed75a3c764facec79c4d3ce3b42d6
William Helton <william_helton2@mymail.eku.edu>
2018-06-02 13:22:11 -0400

    dev deploy test with batch file

--- calibrate-angular/src/app/app.module.ts
+++ calibrate-angular/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { NavbarSimpleComponent } from './components/navbar-simple/navbar-simple.component';
+import { LibraryComponent } from './components/library/library.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginpageComponent,
-    NavbarSimpleComponent
+    NavbarSimpleComponent,
+    LibraryComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
