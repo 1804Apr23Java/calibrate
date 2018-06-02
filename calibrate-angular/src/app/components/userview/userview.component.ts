274251f28aa769db1ee4668ab87d28d3e8bcb2e5
William Helton <william_helton2@mymail.eku.edu>
2018-06-02 14:42:43 -0400

    Simple Routing

---
+++ calibrate-angular/src/app/components/userview/userview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userview',
+  templateUrl: './userview.component.html',
+  styleUrls: ['./userview.component.css']
+})
+export class UserviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
