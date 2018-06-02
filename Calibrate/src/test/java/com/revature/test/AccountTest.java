444958d60fa62db2a2e06fa0a39cde3b616bff9e
rtruong2 <rtruong2@gmu.edu>
2018-06-02 16:40:24 -0400

    testing framework added

---
+++ Calibrate/src/test/java/com/revature/test/AccountTest.java
@@ -0,0 +1,25 @@
+package com.revature.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.revature.repository.AccountRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"beans.xml"})
+public class AccountTest {
+
+	@Autowired
+	private AccountRepository ar;
+
+	@Test
+	public void testAccountRepository() {
+		assertNotNull(ar.getAccounts());
+	}
+	
+}
