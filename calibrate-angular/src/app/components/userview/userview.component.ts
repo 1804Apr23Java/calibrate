283884ce46a3474fb9588ec493254640ca5ff35d
Dave Brewer <dave.guido.brewer@gmail.com>
2018-06-03 18:05:26 -0400

    it kinda works

--- calibrate-angular/src/app/components/userview/userview.component.ts
+++ calibrate-angular/src/app/components/userview/userview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Account } from '../../classes/account';
 import { AccountService } from '../../services/account.service';
+import { Account } from '../../classes/account';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,21 +9,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./userview.component.css']
 })
 export class UserviewComponent implements OnInit {
-  
+
   user: Account;
-  user2: Observable<Account>;
-
-
+  
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit() {
 
-    this.user = new Account(1, 'dave@gmail.com', false, 'chunli', 'DaveB');
+    this.accountService.fetchAccount().subscribe(
+      (account: Account) => {this.user = account}, error => console.log(`Error: ${error} `)
+    );
 
-    this.user2 = this.accountService.fetchAccount();
+    
 
-    console.log(this.user2);
   }
 
 }
--- calibrate-angular/src/app/services/account.service.ts
+++ calibrate-angular/src/app/services/account.service.ts
@@ -3,7 +3,7 @@ import { Account } from '../classes/account';
 import { ACCOUNT } from '../mock-account';
 
 import { Http, Response } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -14,7 +14,9 @@ export class AccountService {
   //inject Http object
   constructor(private http: Http, private httpClient: HttpClient) { }
 
-  public fetchAccountInformation(accountId: number): Observable<Account> {
+  private accountUrl = 'http://localhost:8083/CalibrateNgTest/account/82';
+
+  public fetchAccountInfo(accountId: number): Observable<Account> {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon/${accountId}`).pipe(map((response: Response) => {
       return <Account>response.json();
     }))
@@ -24,8 +26,9 @@ export class AccountService {
 
   public fetchAccount(): Observable<Account> {
     console.log("Fetching Account");
-    return this.httpClient.get<Account>('http://ec2-184-72-131-208.compute-1.amazonaws.com:8080/CalibrateDev/account/82');
-  }
+    return this.httpClient.get<Account>('http://localhost:8083/CalibrateNgTest/account/82');
+  } 
+
 
   getAccount(): Account { 
     
