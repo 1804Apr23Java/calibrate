b27ed3074cfed7fc198d5c0f77a3deac60f0f14d
Dave Brewer <dave.guido.brewer@gmail.com>
2018-06-06 20:14:40 -0400

    oh god janky sessions activated

--- calibrate-angular/src/app/components/login-portal/login-portal.component.ts
+++ calibrate-angular/src/app/components/login-portal/login-portal.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../services/account.service';
 import { Account } from '../../classes/account';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-login-portal',
@@ -10,9 +13,37 @@ import { Observable } from 'rxjs';
 })
 export class LoginPortalComponent implements OnInit {
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private router: Router) { }
+
+  
+  getLogin(): void {
+    this.accountService.getLogin(this.account).subscribe(
+      (account: Account) => { 
+        this.returnedAccount = account; 
+        this.userString = JSON.stringify(this.returnedAccount); 
+        console.log(this.userString) 
+        localStorage.setItem('accountId', this.returnedAccount.accountId.toString());
+        this.router.navigate(['user']);
+      }, 
+        error => { console.log(`Error: ${error} `), localStorage.setItem('accountId','') }
+    );
+
+  }
+
+
+  account = new Account();
+  returnedAccount = new Account();
+  userString = new String();
+
+  onSubmit(accountService) {
+
+    this.getLogin();
+
+  }
 
   ngOnInit() {
+
+   
   }
 
 }
--- calibrate-angular/src/app/services/account.service.ts
+++ calibrate-angular/src/app/services/account.service.ts
@@ -11,17 +11,28 @@ import { map } from 'rxjs/operators';
 })
 export class AccountService {
 
-    //inject Http objects
-    constructor(private http: Http, private httpClient: HttpClient) { }
+  //inject Http objects
+  constructor(private http: Http, private httpClient: HttpClient) { }
 
-    //set URL for api fetching without id 
-    private accountUrl = 'http://ec2-184-72-131-208.compute-1.amazonaws.com:8080/CalibrateNgTest/account';
-  
-    //get account by passing URL + id through http
-    public getAccountById(accountId: number): Observable<Account> {
-      return this.http.get(`${this.accountUrl}/${accountId}`).pipe(map((response: Response) => {
-        return <Account>response.json();
-      }));
-    }
+  //set URL for api fetching without id 
+  private accountUrl = 'http://ec2-184-72-131-208.compute-1.amazonaws.com:8080/CalibrateBackend/account';
+
+
+  //get account by passing URL + id through http
+  public getAccountById(accountId: number): Observable<Account> {
+    return this.http.get(`${this.accountUrl}/${accountId}`).pipe(map((response: Response) => {
+      return <Account>response.json();
+    }));
+  }
+
+  //get account by passing URL + id through http
+  public getLogin(account): Observable<Account> {
+
+    console.log('In getLogin: ' + account.email + ' ' + account.password);
+
+    return this.http.get(`${this.accountUrl}/email/${account.email}/password/${account.password}`).pipe(map((response: Response) => {
+      return <Account>response.json();
+    }));
+  }
 
 }
