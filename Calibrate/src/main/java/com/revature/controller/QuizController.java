b727d56ce249825ae8a55809f71c2527f06eac34
Collin Meaney <collinmeaney375@gmail.com>
2018-06-07 00:21:22 -0400

    Attempted the /generate endpoint for quizzes

--- Calibrate/src/main/java/com/revature/controller/QuizController.java
+++ Calibrate/src/main/java/com/revature/controller/QuizController.java
@@ -1,11 +1,15 @@
 package com.revature.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +32,12 @@ public class QuizController {
 	@GetMapping("/{id}")
 	public ResponseEntity<QuizJSON> getQuiz(@PathVariable int id) {
 		Quiz q = quizService.getQuiz(id);
-		System.out.println(q);
 		return new ResponseEntity<QuizJSON>(btju.quizToJSON(q), HttpStatus.OK);
+	}
+	
+	@PostMapping("/generate")
+	public ResponseEntity<QuizJSON> generateQuiz(@RequestBody String name, List<Integer> libraryIds, int length) {
+		return new ResponseEntity<QuizJSON>(btju.quizToJSON(quizService.generateQuiz(name, libraryIds, length)), HttpStatus.OK);
 	}
 	
 }
